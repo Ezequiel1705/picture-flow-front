c86aea5c9994f9860ddb8f4a0531a05a6c750b82
Ezequiel Colicoy <ezequielcoly1@gmail.com>
2023-12-22 01:37:21 -0300

    proyecto general

--- Backend/src/router/loginRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { signIn, signUp } from '../controller/logincontroller.js';
-
-const router = express.Router();
-
-router.post("/register", signUp);
-router.post("/login", signIn);
-
-export default router;
